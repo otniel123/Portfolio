2ce2b7fc19ee41ec67437c1ac6b73b7581fc442c
Otniel Marques <otnielmarques3@gmail.com>
2024-07-06 01:00:19 -0300

    Fazer menu e troca de cores de botões

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing', {
+    strings: ['Músico', 'Desenvolvedor Back-end', 'Desenvolvedor Front-end', 'Desenvolvedor Java'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+});
